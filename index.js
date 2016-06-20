e5b1d86869902ac42a4764c56c5152fe9aa80145
= <facundo.perez@aerolab.com.ar>
2016-06-20 16:35:12 -0300

    add changes

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+//DATABASE_URL  heroku-postgresql:hobby-dev
 // index.js
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('test-app', 'root', '');
+var sequelize = new Sequelize(
+  process.env.DB_NAME || 'da1r1gs5po0rs7',
+  process.env.DB_USERNAME || 'csvgsbjixximdp',
+  process.env.DB_PASSWORD || 'HUL9pI2oE2C6i5nR3QUko07CTZ', {
+    host: process.env.DB_URL || 'ec2-54-225-195-254.compute-1.amazonaws.com',
+    port: process.env.DB_PORT || '5432',
+
+  }
+);
 
 var User = sequelize.define('user', {
   username: Sequelize.STRING,
@@ -18,7 +27,7 @@ var server = restify.createServer();
 
 server.use(restify.queryParser());
 
-server.listen(8080, function() {
+server.listen(process.env.DB_PORT || 8080, function() {
   console.log('Iniciado');
   console.log('%s listening at %s', server.name, server.url);
 });
